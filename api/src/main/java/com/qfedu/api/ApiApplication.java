cb5c785e6de3743307825bf9ceab775c60912673
shiyl <s19951226>
2022-03-22 00:59:07 +0800

    整合mybatis

--- api/src/main/java/com/qfedu/api/ApiApplication.java
+++ api/src/main/java/com/qfedu/api/ApiApplication.java
@@ -1,9 +1,11 @@
 package com.qfedu.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qfedu.fmmall.dao")         //dao配置，进行扫描
 public class ApiApplication {
 
     public static void main(String[] args) {
